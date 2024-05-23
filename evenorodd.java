2a7b49d686e7e241ff6a840e7a019dcda3950778
ANITHANJALI <anithavanum003@gmail.com>
2024-05-23 13:04:39 +0530

    small_program

---
+++ evenorodd.java
@@ -0,0 +1,16 @@
+class patterns
+{
+public static void main(String[] xyz)
+{
+int num=65;
+int res=num%2;
+System.out.println(res);
+if(res==0)
+{
+System.out.println("even");
+}
+else{
+System.out.println("odd");
+}
+}
+}
